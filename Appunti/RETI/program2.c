3233d73f1847769f188db4dff7f3a1d779d0dc57
jean-pepe <gianpaolo.pepe01@gmail.com>
2025-04-22 10:49:15 +0200

    Update program2.c

--- Appunti/RETI/program2.c
+++ Appunti/RETI/program2.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<errno.h>
@@ -7,32 +9,30 @@
 #include <arpa/inet.h>
 
 int main(){
-	int n;
-	char *request = "GET / \r\n";
-	struct sockaddr_in remote;
-	/*METTO IO RESPONSE DIM = 1000000*/
-	char response[1000000];
-	int s;
 	
-	if((s=socket(AF_INET,SOCK_STREAM,0))==-1);
-	{printf("errno=%d\n"); perror("Socket Fallitto"); return -1;}
+	 int sockID = socket(AF_INET,/*IPV4*/
+                        SOCK_STREAM,/*TCP*/
+						0);
 
+	struct sockaddr_in remote;
 	remote.sin_family = AF_INET;
 	remote.sin_port = htons(80);
 	unsigned char * p_IP=&remote.sin_addr.s_addr;
-	p_IP[0]=142;/*IP SERVER*/
+	p_IP[0]=142;/*IP SERVER  --> E' sbagliato?????*/
 	p_IP[1]=250;
 	p_IP[2]=180;
 	p_IP[3]=3;
 	
-	if (connect(s, (struct sockaddr_in *)&remote,sizeof(struct sockaddr_in))== -1)
-	{perror("Connect Fallitta"); return -1;}
-	write(s,
-		  request,
-		  strlen(request));
-		  int len=0;
-		  for(len=0;(n = read(s,response + len,1000000-len))>0; len+=n);
+	char response[1000001];/*char status_line*/
+	char *request = "GET / \r\n";
+	
+	if(connect(sockID,(struct sockaddr*)&remote,sizeof(remote)) != 0);
+		{perror("connect error \n");exit(EXIT_FAILURE);}
+
+	write(sockID,request,strlen(request));
+		  int n=0,len=0;
+		  for(len=0;(n = read(sockID,response + len,1000000-len))>0; len+=n);
 			if(n==-1){perror("read fallita");return -1;}
-			printf("%s \n", response);
+		printf("%s \n", response);
 	return 0;
 }
